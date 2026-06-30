34356439dbceed755f4df4192692ff57afa367b9
grisha <gredmyr@gmail.com>
2026-06-30 19:51:42 +0300

    add/ db

---
+++ backend/db/data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || '127.0.0.1',
+  port: parseInt(process.env.DB_PORT || '5430', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/db/migrations/*.js'],
+  synchronize: false,
+  logging: true,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
