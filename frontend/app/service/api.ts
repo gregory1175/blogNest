35e5fee2cb2b8a67de444abd78c509f425115e58
grisha <gredmyr@gmail.com>
2026-07-07 22:46:44 +0300

    feat/ add state manager and api service

---
+++ frontend/app/service/api.ts
@@ -0,0 +1,85 @@
+// Регистрация
+export interface ISignUpResponse {
+  refreshToken: string;
+  accessToken: string;
+}
+
+export interface ISignUpModel {
+    name: string,
+    email: string,
+    password: string,
+}
+
+const signUpApi = async (data: ISignUpModel): Promise<ISignUpResponse> => {
+    const res = await fetch('http://127.0.0.1:3000/auth/sign-up', {
+    method: "POST",
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json' 
+        },
+    body: JSON.stringify(data), 
+    })
+
+    if (!res.ok) {
+        throw new Error(`Ошибка сервера: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+export {signUpApi}
+
+// авторизация 
+export interface ISignInResponse {
+  refreshToken: string;
+  accessToken: string;
+}
+
+interface ISingInApi {
+    name: string,
+    email: string,
+    password: string,
+}
+
+const signInApi = async (data: ISingInApi): Promise<ISignInResponse> => {
+    const res = await fetch('http://127.0.0.1:3000/auth/sign-in', {
+    method: "POST",
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json' 
+        },
+    body: JSON.stringify(data), 
+    })
+
+    if (!res.ok) {
+        throw new Error(`Ошибка сервера: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+export {signInApi}
+
+// запрос на обновление токена 
+interface IRefreshTokenApi {
+    refreshToken: string;
+}
+
+const refreshTokenApi = async(data: IRefreshTokenApi): Promise<unknown> => {
+    const res = await fetch('http://127.0.0.1:3000/auth/refresh-token', {
+    method: "POST",
+    credentials: 'same-origin',
+    headers: {
+        'Content-Type': 'application/json' 
+        },
+    body: JSON.stringify(data), 
+    })
+
+    if (!res.ok) {
+        throw new Error(`Ошибка сервера: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+export {refreshTokenApi}
---
+++ frontend/app/store/store.ts
@@ -0,0 +1,58 @@
+// стор для получение и передачи данных из таблицы на бек
+import { create } from "zustand";
+import { signUpApi } from "../service/api";
+
+// общие типы
+
+
+// тип + стор для регистрации 
+
+interface IUseAuthStore  {
+    accessToken: null | string;
+    refreshToken: null | string;
+
+    name: null | string;
+    email: null | string;
+    password: null | string;
+
+    // функция для получения данных для юзера вход/регистрация
+    getUserInfo: (name: string, email: string, password: string) => void;
+    // функция для создания пользователя
+    sendInfoForCreateUser: (name: string, email: string, password: string) => Promise<void>;
+
+}
+
+export const useAuthStore = create<IUseAuthStore>((set) =>({
+    refreshToken: typeof window !== 'undefined' ? localStorage.getItem('refresh_token') : null,
+    accessToken: null, 
+    name: null,
+    email: null, 
+    password: null,
+
+    sendInfoForCreateUser: async (name, email, password) => {
+    try {
+        const model = {
+            name, 
+            email, 
+            password
+        }
+        const res = await signUpApi(model); 
+            set({ 
+                accessToken: res.accessToken,
+                refreshToken: res.refreshToken,
+            });
+    localStorage.setItem('refresh_token', res.refreshToken);  
+    } catch (error: unknown) {
+        throw new Error(`произошла ошибка ${error}`)
+    }
+    },
+
+    getUserInfo: async(name, email, password) => {
+    set({name})
+    set({email})
+    set({password})
+    return    
+    }
+})) 
+
+// тип + стор для авторизации
