0a0a32a21f65d4068edb13482a02b6efb3b9efc0
grisha <gredmyr@gmail.com>
2026-07-08 17:54:48 +0300

    IMPORTAND ADD ALL ACCESS FOR DEVELOP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import cookieParser from 'cookie-parser';
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3001;
 const HOST = process.env.HOST ?? '127.0.0.1';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   await app.listen(PORT, HOST, () => {
     console.log(`Server started on ${PORT}:${HOST}`);
   });
